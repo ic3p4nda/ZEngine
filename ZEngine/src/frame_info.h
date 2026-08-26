4467e7e90ffea9c0317be7819feb458c80d7578b
IcePanda <brunom.tania@gmail.com>
2026-08-26 21:27:26 +0100

    Specular Light

--- ZEngine/src/frame_info.h
+++ ZEngine/src/frame_info.h
@@ -20,6 +20,7 @@ namespace ZEngine
     {
         glm::mat4 projection{1.0f};
         glm::mat4 view{1.0f};
+        glm::mat4 inverseView{1.0f};
         glm::vec4 ambientColor{1.0f, 1.0f, 1.0f, 0.02f};
         PointLight pointLights[MAX_LIGHTS];
         int numLights;
